f959aed86668e2ccd8e17e7a2ffc20a78e918024
changmin Lee / 이창민 <eeeasycode@gmail.com>
2022-07-27 01:42:42 +0900

    학사공지 내용 변경

--- get_notice.py
+++ get_notice.py
@@ -1,31 +1,28 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-from tabulate import tabulate
-
-tabulate.WIDE_CHARS_MODE = False
-
-
-def get_notice():
-    url = "https://www.gachon.ac.kr/kor/3104/subview.do"
-
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        html = response.text
-        soup = BeautifulSoup(html, 'html.parser')
-
-        notices_table = soup.select('table > tbody > tr > td.td-subject > a > strong')
-        date_table = soup.select('table > tbody > tr > td.td-date')
-
-        notice_list = [notice.get_text() for notice in notices_table]
-        date_list = [dates.get_text() for dates in date_table]
-
-        notice_dict = {"작성일": date_list, "제목": notice_list}
-
-        df = pd.DataFrame(notice_dict)
-
-        return tabulate(df, headers='keys', tablefmt='simple', showindex=False)
-
-    else:
-        print(response.status_code)
+# import requests
+# from bs4 import BeautifulSoup
+#
+#
+#
+#
+#
+#
+# def get_notice():
+#     url = "https://www.gachon.ac.kr/kor/3104/subview.do"
+#
+#     response = requests.get(url)
+#
+#     if response.status_code == 200:
+#         html = response.text
+#         soup = BeautifulSoup(html, 'html.parser')
+#
+#         notices_table = soup.select('table > tbody > tr > td.td-subject > a > strong')
+#         date_table = soup.select('table > tbody > tr > td.td-date')
+#
+#         notice_list = [notice.get_text() for notice in notices_table]
+#         date_list = [dates.get_text() for dates in date_table]
+#
+#         for i in range(len(notice_list)):
+#             f'''제목 : {notice_list[i]}\n작성일: {date_list[i]}'''
+#
+#     else:
+#         print(response.status_code)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import academic_calender
 import discord
+import requests
+from bs4 import BeautifulSoup
 import config
-import academic_calender as calender
-import get_notice as notice
 
 client = discord.Client()
 
@@ -16,10 +17,31 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
     if message.content.startswith('!학사공지'):
-        await message.channel.send(notice.get_notice())
+        url = "https://www.gachon.ac.kr/kor/3104/subview.do"
+        response = requests.get(url)
+        if response.status_code == 200:
+            embed = discord.Embed(title="가천대학교 공지사항", description="")
+            html = response.text
+            soup = BeautifulSoup(html, 'html.parser')
+
+            notices_table = soup.select('table > tbody > tr > td.td-subject > a > strong')
+            date_table = soup.select('table > tbody > tr > td.td-date')
+
+            notice_list = [notice.get_text() for notice in notices_table]
+            date_list = [dates.get_text() for dates in date_table]
+
+            for i in range(len(notice_list)):
+                embed.add_field(name="제목", value=notice_list[i])
+                embed.add_field(name="작성일", value=date_list[i] + "\n------------------------------------------------------------------------------",
+                                inline=False)
+        else:
+            print(response.status_code)
+        await message.channel.send(embed=embed)
+
     elif message.content.startswith('!학사일정'):
-        await message.channel.send(calender.Academic_calender())
+        await message.channel.send(academic_calender.Academic_calender())
     elif message.content.startswith('!안녕'):
         await message.channel.send("안녕 ㅎㅎ")
 
